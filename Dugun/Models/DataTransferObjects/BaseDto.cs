0eeb9b8b67ad685ce1c992d81b4aad6776fa22ea
onacione <serkanyavuz.cs@gmail.com>
2016-04-23 02:44:03 +0300

    ilk commit

---
+++ Dugun/Models/DataTransferObjects/BaseDto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Dugun.Models
+{
+    public class BaseDto
+    {
+        #region Common Properties
+
+        private Nullable<DateTime> dtInsert;
+        public Nullable<DateTime> DtInsert
+        {
+            get
+            {
+                if (dtInsert == null)
+                {
+                    return DateTime.Now;
+                }
+                return dtInsert;
+            }
+            set
+            {
+                dtInsert = value;
+            }
+        }
+        
+        public Nullable<DateTime> DtUpdate { get; set; }
+        public Nullable<int> RowStatus { get; set; }
+
+        #endregion
+
+      
+    }
+}
